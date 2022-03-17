5f0cf0d1ef93feeb8e9eaa9ad4a4c0337feeb957
cw <changweitu@gmail.com>
2022-03-17 10:16:37 +0800

    move entity and storeContext to Core and Infrastructure folders

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace API.Entities
+namespace Core.Entities
 {
     public class Product
     {
